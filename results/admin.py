dec0954ca5f057ba92599020e6c50093d2f9328b
Ifiok Ambrose <ifiokambrose@gmail.com>
2024-06-06 10:05:40 +0000

    first commit

---
+++ results/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+# Register your models here.
+from django.contrib.admin import AdminSite
+admin.site.site_header = 'IVAN Schools Administration'
+admin.site.site_title = 'IVAN Schools Administration'
