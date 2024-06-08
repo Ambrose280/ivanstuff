a9d8e6245e74cb5d85af926e4a9faafb0ba73451
Ifiok Ambrose <ifiokambrose@gmail.com>
2024-06-08 19:39:18 +0000

    omor

--- pri2/admin.py
+++ pri2/admin.py
@@ -1,3 +1,135 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+from django.contrib.admin import AdminSite
+admin.site.site_header = 'IVAN Schools Administration'
+admin.site.site_title = 'IVAN Schools Administration'
+
+
+class MathAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class EnglishAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class LiteratureAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class BscBtechAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class ComputerAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class PHEAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class AgricAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class SocialAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class CivicAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class CRKAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class CreativeArtsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class CreativeWritingAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+
+class AbacusAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class SpellingsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class VerbalAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class QuantAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    list_per_page = 100
+    search_fields = ('title',)
+
+admin.site.register(MusicScores, MusicAdmin)
+admin.site.register(HistoryScores, HistoryAdmin)
+admin.site.register(QuantitativeReasoningScores, QuantAdmin)
+admin.site.register(VerbalReasoningScores, VerbalAdmin)
+admin.site.register(SpellingsScores, SpellingsAdmin)
+admin.site.register(AbacusScores, AbacusAdmin)
+admin.site.register(CreativeWritingScores, CreativeWritingAdmin)
+admin.site.register(CreativeArtsScores, CreativeArtsAdmin)
+admin.site.register(CRKScores, CRKAdmin)
+admin.site.register(CivicEducationScores, CivicAdmin)
+admin.site.register(SocialStudiesScores, SocialAdmin)
+admin.site.register(AgriculturalScienceScores, AgricAdmin)
+admin.site.register(PHEScores, PHEAdmin)
+admin.site.register(ComputerStudiesScores, ComputerAdmin)
+admin.site.register(BasicSciTechScores, BscBtechAdmin)
+admin.site.register(EnglishLiteratureScores, LiteratureAdmin)
+admin.site.register(EnglishScores, EnglishAdmin)
+admin.site.register(MathematicsScores, MathAdmin)
--- pri2/models.py
+++ pri2/models.py
@@ -1,57 +1,92 @@
 from django.db import models
 
 # Create your models here.
-class Mathematics(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Mathematics")
+class MathematicsScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Mathematics Scores")
+    class Meta:
+        verbose_name_plural = "Maths Scores"
 
-class English(models.Model):
+class EnglishScores(models.Model):
     title = models.CharField(max_length=50, verbose_name="English Language")
+    class Meta:
+        verbose_name_plural = "English Scores"
     
-class EnglishLiterature(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Literature-In-English")
+class EnglishLiteratureScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Literature Scores")
+    class Meta:
+        verbose_name_plural = "English Literature Scores"
 
-class BasicSciTech(models.Model):
+class BasicSciTechScores(models.Model):
     title = models.CharField(max_length=50, verbose_name="Basic Science & Tech")
+    class Meta:
+        verbose_name_plural = "Basic Science/Tech Scores"
     
-class ComputerStudies(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Computer Studies")
+class ComputerStudiesScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Computer Studies Scores")
+    class Meta:
+        verbose_name_plural = "Computer Studies Scores"
 
-class PHE(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Physical & Health Education")
+class PHEScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Physical & Health Education Scores")
+    class Meta:
+        verbose_name_plural = "PHE Scores"
     
-class AgriculturalScience(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Agricultural Science")
+class AgriculturalScienceScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Agricultural Science Scores")
+    class Meta:
+        verbose_name_plural = "Agric Scores"
 
-class SocialStudies(models.Model):
+class SocialStudiesScores(models.Model):
     title = models.CharField(max_length=50, verbose_name="Social Studies")
+    class Meta:
+        verbose_name_plural = "Social Studies Scores"
     
-class CivicEducation(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Civic Education")
+class CivicEducationScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Civic Education Scores")
+    class Meta:
+        verbose_name_plural = "Civic Education Scores"
 
-class CRK(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Christian Religious Knowledge")
+class CRKScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="CRK Scores")
+    class Meta:
+        verbose_name_plural = "CRK Scores"
     
-class CreativeArts(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Creative Arts")
+class CreativeArtsScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Creative Arts Scores")
+    class Meta:
+        verbose_name_plural = "Creative Arts Scores"
 
-class CreativeWriting(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Creative Handwriting")
+class CreativeWritingScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Creative Writing Scores")
+    class Meta:
+        verbose_name_plural = "Creative Writing Scores"
     
-class Abacus(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Abacus")
+class AbacusScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Abacus Scores")
+    class Meta:
+        verbose_name_plural = "Abacus Scores"
 
-class Spellings(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Spellings")
+class SpellingsScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Spellings Scores")
+    class Meta:
+        verbose_name_plural = "Spellings Scores"
 
-class VerbalReasoning(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Verbal Reasoning")
+class VerbalReasoningScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Verbal Reasoning Scores")
+    class Meta:
+        verbose_name_plural = "Verbal Reasoning Scores"
 
-class QuantitativeReasoning(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Quantitative Reasoning")
+class QuantitativeReasoningScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Quantitative Reasoning Scores")
+    class Meta:
+        verbose_name_plural = "Quantitative Reasoning Scores"
     
-class History(models.Model):
-    title = models.CharField(max_length=50, verbose_name="History")
+class HistoryScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="History Scores")
+    class Meta:
+        verbose_name_plural = "History Scores"
 
-class Music(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Music")
-    
+class MusicScores(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Music Scores")
+    class Meta:
+        verbose_name_plural = "Music Scores"
